1527f684c5f2c27cc0e388e21fc20c469cac575c
Heathcliff Demaie <heathcliff.demaie@univ-lorraine.fr>
2024-03-01 06:08:07 +0100

    Update ppt

---
+++ Class_1/Exercices.py
@@ -0,0 +1,19 @@
+
+## Conditions p77
+year = 2024
+
+if year % 4:
+    # not divisible by 4
+    print("no leap year")
+elif year % 100 == 0 and year % 400 != 0:
+    print("no leap year")
+else:
+    print("leap year")
+
+
+if 1==0:
+    print("A")
+    print("B")
+else:
+    print("OK")
+
